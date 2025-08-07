8a2999ca399cb592fb17ec8174a01d43732ffd84
[Kiran-Patil-rgb] <[kiranpatil5225@gmail.com]>
2025-08-07 21:52:40 +0530

    task

---
+++ jstask/operaters/operaters.js
@@ -0,0 +1,36 @@
+//arithmetics operaters//
+console.log("using arithmetics operaters")
+let num1 = 26
+let num2 = 34
+let sum = num1 + num2
+console.log(sum)
+
+let sum2 = num1 - num2
+console.log(sum2)
+
+let sum3 = num1 * num2
+console.log(sum3)
+
+let sum4 = num1 / num2 
+console.log(sum4)
+
+let sum5 = num1 % num2
+console.log(sum5)
+
+//assignment opraters//
+console.log("using assignment operaters")
+let value = 30
+value += 30
+console.log(value)
+
+value -= 30
+console.log(value)
+
+value *= 30
+console.log(value)
+
+value %= 30
+console.log(value)
+
+value /+ 30
+console.log(value)
