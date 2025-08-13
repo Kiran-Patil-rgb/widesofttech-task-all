c5480dab1634930aa8fe3b2945baeea86500cfb8
[Kiran-Patil-rgb] <[kiranpatil5225@gmail.com]>
2025-08-13 19:20:25 +0530

    file1

---
+++ jstask/string_manipulation/string.js
@@ -0,0 +1,6 @@
+let originalString = "student"; 
+let reversedString = "";    
+for (let i = originalString.length - 1; i >= 0; i--) { 
+  reversedString += originalString[i]; 
+}
+console.log(reversedString); 
