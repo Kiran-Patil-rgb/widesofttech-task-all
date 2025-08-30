c17ba00829c2a397a13672b1cdb87071fc35f540
[Kiran-Patil-rgb] <[kiranpatil5225@gmail.com]>
2025-08-30 11:22:50 +0530

    innerHtML

--- jstask/DOM/dom.js
+++ jstask/DOM/dom.js
@@ -1,3 +1,4 @@
+//add border or remove border//
 document.addEventListener("DOMContentLoaded", () => {
 
     let myDiv = document.getElementById("id1");
@@ -16,5 +17,66 @@ document.addEventListener("DOMContentLoaded", () => {
     button.addEventListener("click", Includes);
 });
 
+//show and hide//
+let togglebutton1 = document.getElementById("btn2");
+let bluebox1 = document.getElementById("id2");
+togglebutton1.addEventListener("click",()=>{
+    bluebox1.classList.toggle("hide");
+});
+
+//another way to hide and show//
+// let togglebutton = document.getElementById("btn2");
+// let bluebox = document.getElementById("id2");
+
+// togglebutton.addEventListener("click", () => {
+    
+//     if (bluebox.style.display === "none") {
+       
+//         bluebox.style.display = "block";
+//     } else {
+//         bluebox.style.display = "none";
+//     }
+// });
+//table//
+const button = document.getElementById("btn3");
+const tableBody = document.querySelector("#table1 tbody");
+
+const tabledata = [
+    {
+        name: "kiran",
+        id: 2001,
+    },
+    {
+        name: "pranay",
+        id : 2002,
+    },
+    {
+        name: "sejal",
+        id: 2003,
+    },
+    {
+        name: "aryan",
+        id: 2004,
+    }
+];
+
+let rowIndex = 0;
 
 
+button.addEventListener("click", () => {
+   
+    if (rowIndex >= tabledata.length) {
+     window.alert("table is finished");
+     
+        return;
+    }
+    const rowData = tabledata[rowIndex];
+
+    tableBody.innerHTML += `
+        <tr>
+            <td>${rowData.name}</td>
+            <td>${rowData.id}</td>
+        </tr>
+    `;
+    rowIndex++;
+});
